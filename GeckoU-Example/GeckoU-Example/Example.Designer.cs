953ec99b0a4445cacd664604b38f512ef2b6e946
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-11-12 17:06:01 +0900

    GeckoU Example

---
+++ GeckoU-Example/GeckoU-Example/Example.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GeckoU_Example
+{
+    partial class Example
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.WiiUIPv4Text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(304, 131);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(161, 57);
+            this.DisconnectButton.TabIndex = 5;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(111, 131);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(161, 57);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // WiiUIPv4Text
+            // 
+            this.WiiUIPv4Text.Location = new System.Drawing.Point(111, 75);
+            this.WiiUIPv4Text.Name = "WiiUIPv4Text";
+            this.WiiUIPv4Text.Size = new System.Drawing.Size(354, 25);
+            this.WiiUIPv4Text.TabIndex = 3;
+            this.WiiUIPv4Text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Example
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 262);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.WiiUIPv4Text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Example";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox WiiUIPv4Text;
+    }
+}
+
---
+++ GeckoU-Example/GeckoU-Example/Example.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using com.wiiplaza.geckou;
+
+namespace GeckoU_Example
+{
+    public partial class Example : Form
+    {
+        public Example()
+        {
+            InitializeComponent();
+            this.DisconnectButton.Enabled = false;
+        }
+
+        public static GeckoUCore GeckoU;
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+
+            /*
+             * The connection process starts here
+             * 7331 is the port number of WiiUTCPGecko
+             * Basically, you can use it as it is
+             */
+
+            try
+            {
+                GeckoU = new GeckoUCore(WiiUIPv4Text.Text, 7331);
+                GeckoU.GUC.Connect();
+                this.ConnectButton.Text = "Connected";
+                this.ConnectButton.Enabled = false;
+                this.DisconnectButton.Enabled = true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message,"GeckoU");
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+
+            GeckoU.GUC.Close();
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.Enabled = true;
+            this.DisconnectButton.Enabled = false;
+        }
+    }
+}
