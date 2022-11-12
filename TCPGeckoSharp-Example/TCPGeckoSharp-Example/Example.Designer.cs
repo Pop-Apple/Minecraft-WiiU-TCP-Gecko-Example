4e97294021190292fc5ef03db23eea21d29e2a57
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-11-12 17:05:21 +0900

    TCPGeckoSharp Example

---
+++ TCPGeckoSharp-Example/TCPGeckoSharp-Example/Example.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TCPGeckoSharp_Example
+{
+    partial class Example
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WiiUIPv4Text = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WiiUIPv4Text
+            // 
+            this.WiiUIPv4Text.Location = new System.Drawing.Point(111, 72);
+            this.WiiUIPv4Text.Name = "WiiUIPv4Text";
+            this.WiiUIPv4Text.Size = new System.Drawing.Size(354, 25);
+            this.WiiUIPv4Text.TabIndex = 0;
+            this.WiiUIPv4Text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(111, 128);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(161, 57);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(304, 128);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(161, 57);
+            this.DisconnectButton.TabIndex = 2;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // Example
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 262);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.WiiUIPv4Text);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Example";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WiiUIPv4Text;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+    }
+}
+
---
+++ TCPGeckoSharp-Example/TCPGeckoSharp-Example/Example.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using TCPGeckoSharp;
+
+namespace TCPGeckoSharp_Example
+{
+    public partial class Example : Form
+    {
+        public Example()
+        {
+            InitializeComponent();
+            this.DisconnectButton.Enabled = false;
+        }
+
+        public TCPGecko gecko;
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            this.gecko = new TCPGecko(WiiUIPv4Text.Text, 7331);
+
+            /* 
+             * The connection process starts here
+             * 7331 is the port number of WiiUTCPGecko
+             * Basically, you can use it as it is
+             */
+
+            try
+            {
+                this.gecko.Connect();
+                this.ConnectButton.Text = "Connected";
+                this.ConnectButton.Enabled = false;
+                this.DisconnectButton.Enabled = true;
+            }
+            catch (ETCPGeckoException ex)
+            {
+                //Connection Failure
+                MessageBox.Show(ex.Message,"TCPGeckoSharp");
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                //IP is unclear
+                MessageBox.Show(ex.Message,"TCPGeckoSharp");
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.gecko.Disconnect();
+                this.ConnectButton.Text = "Connect";
+                this.ConnectButton.Enabled = true;
+                this.DisconnectButton.Enabled = false;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
